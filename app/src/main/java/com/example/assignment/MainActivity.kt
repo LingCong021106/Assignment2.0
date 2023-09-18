327607d7af591fcbef0f33f66c8273b61a76e1db
LingCong <lingc-wm20@student.tarc.edu.my>
2023-09-18 19:26:16 +0800

    initial

--- app/src/main/java/com/example/assignment/MainActivity.kt
+++ app/src/main/java/com/example/assignment/MainActivity.kt
@@ -6,6 +6,37 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.user_home)
+        //setContentView(R.layout.user_home)
+
+//        val drawerLayout : DrawerLayout = findViewById(R.id.admin_home)
+//        val navView : NavigationView = findViewById(R.id.nav_view)
+//
+//        toggle = ActionBarDrawerToggle(this,drawerLayout, R.string.open, R.string.close)
+//        drawerLayout.addDrawerListener(toggle)
+//        toggle.syncState()
+//
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//
+//        navView.setNavigationItemSelectedListener {
+//
+//            when(it.itemId){
+//                R.id.admin_nav_dashboard -> Toast.makeText(applicationContext,"Clicked", Toast.LENGTH_LONG).show()
+//            }
+//
+//            true
+//
+//        }
+
+//    }
+
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//
+//        if(toggle.onOptionsItemSelected(item)){
+//            return true
+//        }
+//
+//        return super.onOptionsItemSelected(item)
+//    }
     }
 }
