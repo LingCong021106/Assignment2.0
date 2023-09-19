2b02eeee7b92c704c715b5e963551327b08fa291
LingCong <lingc-wm20@student.tarc.edu.my>
2023-09-19 21:55:31 +0800

    user home fragment

--- app/src/main/java/com/example/assignment/MainActivity.kt
+++ app/src/main/java/com/example/assignment/MainActivity.kt
@@ -2,11 +2,48 @@ package com.example.assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.widget.ButtonBarLayout
+import androidx.fragment.app.Fragment
+import com.example.assignment.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val userHomeFragment = UserHomeFragment()
+    private val eventFragment = EventFragment()
+    private val donateFragment = DonateFragment()
+    private val profileFragment = ProfileFragment()
+    private
+    lateinit var bottomNav : BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.user_home)
+        loadFragment(UserHomeFragment())
+        bottomNav = findViewById(R.id.bottom_navigation) as BottomNavigationView
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    loadFragment(UserHomeFragment())
+                    true
+                }
+                R.id.donate -> {
+                    loadFragment(DonateFragment())
+                    true
+                }
+                R.id.event -> {
+                    loadFragment(EventFragment())
+                    true
+                }
+                R.id.profile -> {
+                    loadFragment(ProfileFragment())
+                    true
+                }
+
+                else -> {true}
+            }
+        }
+
         //setContentView(R.layout.user_home)
 
 //        val drawerLayout : DrawerLayout = findViewById(R.id.admin_home)
@@ -38,5 +75,11 @@ class MainActivity : AppCompatActivity() {
 //
 //        return super.onOptionsItemSelected(item)
 //    }
+    }
+
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container,fragment)
+        transaction.commit()
     }
 }
