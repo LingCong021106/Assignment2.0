a30128ad3736da32d4b24b89c4c08bdad39c8441
tommy <jhtan-wm20@student.tarc.edu.my>
2023-09-22 14:17:44 +0800

    user_registration

---
+++ app/src/main/java/com/example/assignment/RegisterAccount.kt
@@ -0,0 +1,12 @@
+package com.example.assignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterAccount : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_register_account)
+
+    }
+}
