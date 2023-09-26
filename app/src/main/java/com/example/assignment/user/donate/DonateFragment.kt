790a135bb8b4d1f6385234ca7347c273cfddc8cd
LingCong <lingc-wm20@student.tarc.edu.my>
2023-09-26 21:36:30 +0800

    fragment update

--- app/src/main/java/com/example/assignment/user/donate/DonateFragment.kt
+++ app/src/main/java/com/example/assignment/user/donate/DonateFragment.kt
@@ -35,7 +35,7 @@ class DonateFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_donate, container, false)
+        return inflater.inflate(R.layout.fragment_user_donate, container, false)
     }
 
     companion object {
